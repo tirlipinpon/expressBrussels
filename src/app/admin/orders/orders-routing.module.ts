1a515b4679c8284c95b1e86eecc471a1ce4ad0a7
tony <tirlipinpon@hotmail.com>
2019-02-11 15:19:36 +0100

    orders list component lazy

--- src/app/admin/orders/orders-routing.module.ts
+++ src/app/admin/orders/orders-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {OrdersListComponent} from "./orders-list/orders-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin/orders',
+    component: OrdersListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/admin/orders/orders.module.ts
+++ src/app/admin/orders/orders.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { OrdersRoutingModule } from './orders-routing.module';
 import { OrdersListComponent } from './orders-list/orders-list.component';
+import {ReactiveFormsModule, FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [OrdersListComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     OrdersRoutingModule
   ]
 })
