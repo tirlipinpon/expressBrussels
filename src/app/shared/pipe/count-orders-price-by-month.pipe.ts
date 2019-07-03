0ce997ddd968c43cd05bb176502a41e2dff08c3d
tony <tirlipinpon@hotmail.com>
2019-07-03 20:16:57 +0200

    live relaod + facture price month tofixed(2)

--- src/app/shared/pipe/count-orders-price-by-month.pipe.ts
+++ src/app/shared/pipe/count-orders-price-by-month.pipe.ts
@@ -1,4 +1,4 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
   name: 'countOrdersPriceByMonth'
@@ -8,14 +8,13 @@ export class CountOrdersPriceByMonthPipe implements PipeTransform {
   transform(value: any, args?: any): any {
     if (!value || !args) return null;
 
-    return args.map(data =>
-      {
+    return args.map(data => {
         if (data.created.slice(5, 7) === value && +data.created.slice(0, 4) === +(new Date()).getFullYear()) {
           return +data.price
         }
         return 0
       }
-    ).reduce((acc, value) => acc + value, 0);
+    ).reduce((acc, value) => acc + value);
 
   }
 }
